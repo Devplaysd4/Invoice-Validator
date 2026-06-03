a6e9f276f539f41e0a3c14d5e971648e33b75b0f
devplaysd4 <devplaysd4@gmail.com>
2026-06-03 16:51:56 +0530

    3rd june made and tried validation logic on sample data

---
+++ validator.py
@@ -0,0 +1,59 @@
+import pandas as pd
+from datetime import datetime 
+# loads csv to see first few rows
+invoice_data = pd.read_csv("sample_invoice.csv")
+print(invoice_data.head())
+# column names
+print(invoice_data.columns.tolist())
+#iterates for row number and row data
+for index,row in invoice_data.iterrows():
+    print(row["amount"])
+def validate_date(date):
+    try:
+        datetime.strptime(date,"%d/%m/%Y")
+        return True
+    except ValueError:
+        return False    
+
+def validate_amount(amount):
+    try:
+        amount = float(amount)
+        if amount <= 0:
+            return False
+        return True
+    except ValueError:
+        return False
+# def validate_qty(qty):
+    try:
+        qty = int(qty)
+        if qty <=0:
+            return False
+        return True
+    except ValueError:
+        return False
+
+# def validate_email(email):
+    if"@"in email and"." in email:
+        return True
+    return False
+
+
+errors = []
+for index, row in invoice_data.iterrows():
+
+    if not validate_date(row["date"]):
+        errors.append(f"Row {index}: Invalid Date")
+
+    if not validate_amount(row["amount"]):
+        errors.append(f"Row {index}: Invalid Amount")
+
+    # if not validate_qty(row["qty"]):
+        # errors.append(f"Row {index}: Invalid Quantity")
+
+    # if not validate_email(row["email"]):
+        # errors.append(f"Row {index}: Invalid Email")
+
+print("\nValidation Report")
+
+for error in errors:
+    print(error)
