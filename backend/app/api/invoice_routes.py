5f9dbf12b5274c540059f844803e03fa4921bb74
devplaysd4 <devplaysd4@gmail.com>
2026-07-09 23:50:09 +0530

    m2 p4 done m2 done

--- backend/app/api/invoice_routes.py
+++ backend/app/api/invoice_routes.py
@@ -1,3 +1,6 @@
+from datetime import date
+from typing import Optional
+
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
@@ -17,3 +20,25 @@ def get_db():
 def get_all_invoices(db: Session=Depends(get_db)):
     invoices =db.query(Invoice).all()
     return invoices
+
+@router.post("/invoices")
+def create_invoice(invoice_number: str,
+    vendor: str,
+    invoice_date:date,
+    amount:float,
+    status:str,
+    validation_errors:Optional[str] = None,
+    db:Session =Depends(get_db)
+):
+    new_invoice = Invoice(
+        invoice_number=invoice_number,
+        vendor=vendor,
+        invoice_date=invoice_date,
+        amount=amount,
+        status=status,
+        validation_errors=validation_errors
+    )
+    db.add(new_invoice)
+    db.commit()
+    db.refresh(new_invoice)
+    return new_invoice
